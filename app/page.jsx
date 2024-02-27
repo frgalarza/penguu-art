7f28c4d1e82dc241a9cfc21dc951714fe88644ce
Franco Galarza <francig4444@gmail.com>
2024-02-27 00:48:10 -0300

    Estilos inicio

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,20 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import icono from '../public/penguu icono.png'
+import { FaTiktok } from "react-icons/fa";
+import { FiInstagram } from "react-icons/fi";
+import { FaXTwitter } from "react-icons/fa6";
 
 export default function Home() {
   return (
     <main className={styles.main}>
         <header className={styles.pannel}>
-          <Image className={styles.figure} src={icono} height={"300"} width={"400"} alt='penguu icon'/>
+          <Image className={styles.figure} src={icono} height={"300"} width={"600"} alt='penguu icon'/>
           <h2 className={styles.subTitle}>Comisiones abiertas</h2>
           <ul className={`${styles.ul} ${styles.social}`}>
-            <li><a href="https://www.tiktok.com/@penguu.art" target='_blank'>TikTok</a></li>
-            <li><a href="https://www.instagram.com/penguu.art/?hl=es-la" target='_blank'>Instagram</a></li>
-            <li><a href="https://twitter.com/penguu_art" target='_blank'>Twitter</a></li>
+            <li><a href="https://www.tiktok.com/@penguu.art" target='_blank'><FaTiktok /></a></li>
+            <li><a href="https://www.instagram.com/penguu.art/?hl=es-la" target='_blank'><FiInstagram/></a></li>
+            <li><a href="https://twitter.com/penguu_art" target='_blank'><FaXTwitter/></a></li>
           </ul>
         </header>
         <div className={styles.content}>
