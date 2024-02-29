862ae9e423435330b4f817e8d08f1d496ac4d1dc
Franco Galarza <francig4444@gmail.com>
2024-02-29 10:21:32 -0300

    icono sobre mi

--- app/page.jsx
+++ app/page.jsx
@@ -8,6 +8,7 @@ import { FaXTwitter } from "react-icons/fa6";
 import { FaHandshake } from "react-icons/fa6";
 import { FaSackDollar } from "react-icons/fa6";
 import { FaPaintBrush } from "react-icons/fa";
+import { MdFace3 } from "react-icons/md";
 
 export default function Home() {
   return (
@@ -24,8 +25,8 @@ export default function Home() {
         <div className={styles.content}>
           <h1 className={styles.title}>Penguu Art ❤</h1>
           <ul  className={styles.ul}>
-            <li className={styles.li}><Link href="/about" className={styles.button}>✦ Sobre mi ✦</Link></li>
-            <li className={styles.li}><Link href="/portfolio" className={styles.button}><FaPaintBrush className={styles.icon}/>✦ Mis trabajos ✦</Link></li>
+            <li className={styles.li}><Link href="/about" className={styles.button}><MdFace3 className={styles.icon}/>✦ Sobre mi ✦</Link></li>
+            <li className={styles.li}><Link href="/portfolio" className={styles.button}><FaPaintBrush className={styles.icon}/>✦ Comisiones ✦</Link></li>
             <li className={styles.li}><Link href="/prices" className={styles.button}><FaSackDollar className={styles.icon}/>✦ Precios ✦</Link></li>
             <li className={styles.li}><Link href="/temrm-and-conditions" className={styles.button}><FaHandshake className={styles.icon}/>✦ Terminos y condiciones ✦</Link></li>
           </ul>
