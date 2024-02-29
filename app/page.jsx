77be8b194002040f8bba7da123f7a87a4e667042
Franco Galarza <francig4444@gmail.com>
2024-02-29 10:06:40 -0300

    estilos botones social

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link';
 import styles from './page.module.css'
 import icono from '../public/penguu icono.png'
 import { FaTiktok } from "react-icons/fa";
 import { FiInstagram } from "react-icons/fi";
 import { FaXTwitter } from "react-icons/fa6";
+import { FaHandshake } from "react-icons/fa6";
+import { FaSackDollar } from "react-icons/fa6";
+import { FaPaintBrush } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -12,18 +16,18 @@ export default function Home() {
           <Image className={styles.figure} src={icono} height={"300"} width={"600"} alt='penguu icon'/>
           <h2 className={styles.title}>Comisiones abiertas</h2>
           <ul className={`${styles.ul} ${styles.social}`}>
-            <li><a href="https://www.tiktok.com/@penguu.art" target='_blank'><FaTiktok /></a></li>
-            <li><a href="https://www.instagram.com/penguu.art/?hl=es-la" target='_blank'><FiInstagram/></a></li>
-            <li><a href="https://twitter.com/penguu_art" target='_blank'><FaXTwitter/></a></li>
+            <li className={styles.li_soc}><a className={styles.a_soc} href="https://www.tiktok.com/@penguu.art" target='_blank'><FaTiktok /></a></li>
+            <li className={styles.li_soc}><a className={styles.a_soc} href="https://www.instagram.com/penguu.art/?hl=es-la" target='_blank'><FiInstagram/></a></li>
+            <li className={styles.li_soc}><a className={styles.a_soc} href="https://twitter.com/penguu_art" target='_blank'><FaXTwitter/></a></li>
           </ul>
         </header>
         <div className={styles.content}>
           <h1 className={styles.title}>Penguu Art ❤</h1>
           <ul  className={styles.ul}>
-            <li className={styles.li}><button className={styles.button}>Sobre mi</button></li>
-            <li className={styles.li}><button className={styles.button}>Mis trabajos</button></li>
-            <li className={styles.li}><button className={styles.button}>Precios</button></li>
-            <li className={styles.li}><button className={styles.button}>Terminos y condiciones</button></li>
+            <li className={styles.li}><Link href="/about" className={styles.button}>✦ Sobre mi ✦</Link></li>
+            <li className={styles.li}><Link href="/portfolio" className={styles.button}><FaPaintBrush className={styles.icon}/>✦ Mis trabajos ✦</Link></li>
+            <li className={styles.li}><Link href="/prices" className={styles.button}><FaSackDollar className={styles.icon}/>✦ Precios ✦</Link></li>
+            <li className={styles.li}><Link href="/temrm-and-conditions" className={styles.button}><FaHandshake className={styles.icon}/>✦ Terminos y condiciones ✦</Link></li>
           </ul>
         </div>
     </main>
